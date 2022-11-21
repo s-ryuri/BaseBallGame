b154a3c78ac274f8bb43889da8a52ea8cbb2e7f2
s-ryrui <fbfbf1@naver.com>
2022-11-22 01:50:55 +0900

    Format : 코드 포맷 변경

--- src/test/java/baseball/StrikeTest.java
+++ src/test/java/baseball/StrikeTest.java
@@ -1,6 +1,5 @@
 package baseball;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,8 +11,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StrikeTest {
 
     private Strike strike;
-    private static final List<String> RANDOM_NUMBERS = Arrays.asList("1","2","3");
-    private static final List<String> USER_NUMBERS = Arrays.asList("1","2","3");
+    private static final List<String> RANDOM_NUMBERS = Arrays.asList("1", "2", "3");
+    private static final List<String> USER_NUMBERS = Arrays.asList("1", "2", "3");
+
     @BeforeEach
     void setUp() {
         strike = new Strike();
@@ -21,12 +21,11 @@ public class StrikeTest {
 
     @Test
     void 같은위치_같은숫자이면_스트라이크() {
-        assertThat(strike.matchNumber(RANDOM_NUMBERS,0,"1")).isTrue();
+        assertThat(strike.matchNumber(RANDOM_NUMBERS, 0, "1")).isTrue();
     }
 
     @Test
     void 스트라이크_개수_세기() {
-
-        assertThat(strike.getStrikeCount(RANDOM_NUMBERS,USER_NUMBERS)).isEqualTo(3);
+        assertThat(strike.getStrikeCount(RANDOM_NUMBERS, USER_NUMBERS)).isEqualTo(3);
     }
 }
