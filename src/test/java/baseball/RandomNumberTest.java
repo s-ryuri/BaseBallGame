14c1263a93ade708715fcd1e06a7f7c0c05632a9
s-ryrui <fbfbf1@naver.com>
2022-11-22 02:11:44 +0900

    Refactor: @BeforEach 추가

--- src/test/java/baseball/RandomNumberTest.java
+++ src/test/java/baseball/RandomNumberTest.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -10,18 +11,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RandomNumberTest {
 
+    private RandomNumber randomNumber;
+    private List<String> randomNumbers;
+
+    @BeforeEach
+    void setUp() {
+        randomNumber = new RandomNumber();
+        randomNumbers = randomNumber.generateNumber();
+    }
+
     @Test
     void 숫자_3개를_가진다() {
-        final RandomNumber randomNumber = new RandomNumber();
-        List<String> randomNumbers = randomNumber.generateNumber();
-
         assertThat(randomNumbers.size()).isEqualTo(3);
     }
 
     @Test
     void 숫자의_범위는_1부터_9까지이다() {
-        final RandomNumber randomNumber = new RandomNumber();
-        List<String> randomNumbers = randomNumber.generateNumber();
         final List<Integer> collect = randomNumbers.stream()
                                                    .map(Integer::parseInt)
                                                    .toList();
@@ -30,9 +35,6 @@ public class RandomNumberTest {
 
     @Test
     void 중복된_숫자가_없다() {
-        final RandomNumber randomNumber = new RandomNumber();
-        List<String> randomNumbers = randomNumber.generateNumber();
-
         final Set<Integer> integers = randomNumbers.stream()
                                                    .map(Integer::parseInt)
                                                    .collect(Collectors.toSet());
